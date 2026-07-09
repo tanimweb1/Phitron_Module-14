e2276ad9c4f03f89096f30fc3a16763028708310
Shahriar Mahmud Tanim <157669438+tanimweb1@users.noreply.github.com>
2026-07-09 12:50:24 +0600

    Add files via upload

---
+++ return+no_parameter.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int sum(){
+
+    int a,b;
+    scanf("%d%d",&a,&b);
+int ans = a+b;
+return ans;
+
+}
+int main(){
+
+int val =  sum();   
+printf("%d",val);    
+
+    
+return 0;
+}
